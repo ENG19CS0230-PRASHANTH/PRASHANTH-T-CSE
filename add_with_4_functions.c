5c983502dd09c25fe77bb4b68c96fead6787a2dd
ENG19CS0230-PRASHANTH <76095197+ENG19CS0230-PRASHANTH@users.noreply.github.com>
2021-02-04 17:57:48 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -1,1 +1,12 @@
 //Write a program to add two user input numbers using 4 functions.
+#include<stdio.h>
+Int main()
+{
+Float h,b,d,vol;
+printf(“Enter height,breadth,depth :”);
+scanf(“%f%f%f”,&h,&b,&d);
+vol=((h*d)+d)/(3*b);
+printf(“volume of the tromboloid is %f”,vol);
+return 0;
+
+}
