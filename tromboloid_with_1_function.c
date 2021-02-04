d7586e8fb09298c01974ae68e9c4110575b1b9ba
ENG19CS0230-PRASHANTH <76095197+ENG19CS0230-PRASHANTH@users.noreply.github.com>
2021-02-04 18:49:42 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -1,1 +1,19 @@
 //Write a program to find the volume of a tromboloid using one function
+   #include<stdio.h>
+float addition (float num1 , float num2)
+{
+  float sum;
+  sum = num1 + num2;
+ return sum;
+}
+ int main()
+{
+float number1,number2,total;
+printf(“Enter the two numbers :”);
+scanf(“%f%f”, number1 , number);
+total= addition(number 1, number 2);
+printf(“%.2f+%.2f\n”,number 1,number 2,total);
+return 0;
+
+
+}
