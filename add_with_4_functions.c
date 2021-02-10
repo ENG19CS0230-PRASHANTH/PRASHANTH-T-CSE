5420bac7b78fa7a96ecaea66540a3d10159fd8f4
ENG19CS0230-PRASHANTH <76095197+ENG19CS0230-PRASHANTH@users.noreply.github.com>
2021-02-10 11:36:00 +0530

    Update add_with_4_functions.c

--- add_with_4_functions.c
+++ add_with_4_functions.c
@@ -1,12 +1,36 @@
 //Write a program to add two user input numbers using 4 functions.
 #include<stdio.h>
+
+int first_num()
+{
+int num;
+printf("Enter first number ");
+scanf("%d",&num);
+return num;
+}
+
+int second_num()
+{
+int num;
+printf("enter second number ");
+scanf("%d",&num);
+return num;
+}
+
+int add_two_num(int first_num,int second_num)
+{ 
+return (first_num+second_num);
+}
+int print(int sum)
+{
+ printf("sum of the 2 numbers is = %d\n",sum);
+}
+
 int main()
 {
-float h,b,d,vol;
-printf(“Enter height,breadth,depth :”);
-scanf(“%f%f%f”,&h,&b,&d);
-vol=((h*d)+d)/(3*b);
-printf(“volume of the tromboloid is %f”,vol);
+int a=first_num();
+int b=second_num();
+int sum=add_two_num(a,b);
+print(sum);
 return 0;
-
 }
