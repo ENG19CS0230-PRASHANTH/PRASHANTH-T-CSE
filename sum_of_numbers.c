d734da279197d239e9a88efbfacf421f579371fa
ENG19CS0230-PRASHANTH <76095197+ENG19CS0230-PRASHANTH@users.noreply.github.com>
2021-04-06 13:53:23 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,1 +1,49 @@
 //Write a program to find the sum of n different numbers using 4 functions
+#include<stdio.h>
+#include<stdlib.h>
+struct variables
+{
+    int a[10];
+    int n;
+};
+struct variables input();
+int add_sum(struct variables var);
+struct variables output(struct variables var,int sum);
+int main()
+{
+    struct variables values;
+    int sum_add;
+    values=input();
+    sum_add=add_sum(values);
+    output(values,sum_add);
+}
+
+struct variables input()
+{
+    struct variables var;
+    int i;
+    printf("Enter no of values:");
+    scanf("%d",&var.n);
+    for(i=0;i<var.n;i++)
+    {
+        printf("\nEnter number:");
+        scanf("%d",&var.a[i]);
+    }
+    return var;
+}
+
+int add_sum(struct variables var)
+{
+    int i;
+    int sum=0;
+    for(i=0;i<var.n;i++)
+    {
+        sum+=var.a[i];
+    }
+    return sum;
+}
+
+struct variables output(struct variables var,int sum)
+{
+    printf("\nSum of %d numbers is %d\n",var.n,sum);
+}
